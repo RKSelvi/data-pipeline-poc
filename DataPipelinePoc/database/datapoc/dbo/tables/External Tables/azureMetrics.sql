4a8bb9574c6b304d0517259121c873f462559188
Selvi Kalaiselvi <selvi.kalaiselvi@appliedis.com>
2021-02-07 16:17:46 -0500

    Fix external table

--- DataPipelinePoc/database/datapoc/dbo/tables/External Tables/azureMetrics.sql
+++ DataPipelinePoc/database/datapoc/dbo/tables/External Tables/azureMetrics.sql
@@ -1,16 +1,6 @@
 -- drop external table azureMetrics;
 CREATE EXTERNAL TABLE [dbo].[azureMetrics]
-( 	[TenantId] varchar(100) ,
-	[SourceSystem] varchar(50) ,
-	[TimeGenerated] varchar(50) ,
-	[ResourceId] varchar(400) ,
-	[Resource] varchar(100) ,
-	[ResourceGroup] varchar(100) ,
-	[ResourceProvider] varchar(100) ,
-	[SubscriptionId] varchar(100) ,
-	[MetricName] varchar(100) ,	
-	[Type] varchar(50) ,
-	[_ResourceId] varchar(400)
+( 	[name] varchar(100)
 )
 WITH
  (
