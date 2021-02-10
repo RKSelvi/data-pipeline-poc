15096a4c674bfa8d9a030bfecd2d627247dabeff
Selvi Kalaiselvi <selvi.kalaiselvi@appliedis.com>
2021-02-09 23:35:27 -0500

    Update

--- DataPipelinePoc/database/datapoc/dbo/tables/External Tables/azureMetrics.sql
+++ DataPipelinePoc/database/datapoc/dbo/tables/External Tables/azureMetrics.sql
@@ -3,7 +3,7 @@ CREATE EXTERNAL TABLE [dbo].[azureMetrics]
 ( 	[tablename] VARCHAR(100),
     [name]      VARCHAR(50),
     [type]      VARCHAR(50),
-    [items]     VARCHAR2(4000)
+    [items]     VARCHAR(4000)
 )
 WITH
  (
